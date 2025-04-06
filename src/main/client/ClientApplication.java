5b77b4eb1f47299c6db2be0b966ad412a9fe4795
Junseo Bae <ferrater1013@gmail.com>
2025-04-07 01:02:03 +0900

    fix(client): 클라이언트 종료 시 블록 스레드 미종료 문제 해결

--- src/main/client/ClientApplication.java
+++ src/main/client/ClientApplication.java
@@ -14,13 +14,11 @@ public class ClientApplication {
     private final static int PORT = 1234;
 
     public static void main(String[] args) {
-        try (Socket socket = new Socket();
+
+        try (Socket socket = new Socket("localhost", PORT);
              DataInputStream input = new DataInputStream(socket.getInputStream());
              DataOutputStream output = new DataOutputStream(socket.getOutputStream())
         ) {
-            socket.connect(new InetSocketAddress("localhost", PORT), 5000);
-            log("소켓 연결: " + socket);
-
             Thread thread = new Thread(new ReceiveTask(input), "Receive");
             thread.start();
 
@@ -28,8 +26,11 @@ public class ClientApplication {
             while (true) {
                 String command = scanner.nextLine();
                 output.writeUTF(command);
+                if (command.equals("exit")) {
+                    thread.interrupt();
+                    break;
+                }
             }
-
         } catch (IOException e) {
             log(e);
         }
